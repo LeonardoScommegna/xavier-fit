42327187e22673176d90f1c700a18f59a04cbe85
Federico Teotini <templarblack.fed03@gmail.com>
2019-09-09 11:37:27 +0200

    Finished ContextualTestRunner

---
+++ tester/src/main/java/com/fteotini/amf/tester/providers/JUnit5/ContextualTestRunner.java
@@ -0,0 +1,49 @@
+package com.fteotini.amf.tester.providers.JUnit5;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.util.Set;
+import java.util.function.Supplier;
+
+class ContextualTestRunner {
+    private final Set<Path> additionalClassPaths;
+    private final Supplier<Thread> currentThreadProvider;
+
+    ContextualTestRunner(Set<Path> additionalClassPaths) {
+        this(additionalClassPaths, Thread::currentThread);
+    }
+
+    ContextualTestRunner(Set<Path> additionalClassPaths, Supplier<Thread> currentThreadProvider) {
+        this.additionalClassPaths = additionalClassPaths;
+        this.currentThreadProvider = currentThreadProvider;
+    }
+
+    <T> T run(Supplier<T> runnable) {
+        var currentThread = currentThreadProvider.get();
+
+        var original = currentThread.getContextClassLoader();
+        var newClassLoader = buildNewClassLoader(original);
+
+        try {
+            currentThread.setContextClassLoader(newClassLoader);
+            return runnable.get();
+        } finally {
+            currentThread.setContextClassLoader(original);
+        }
+    }
+
+    private ClassLoader buildNewClassLoader(ClassLoader original) {
+        var classPathURLs = additionalClassPaths.stream().map(ContextualTestRunner::toURL).toArray(URL[]::new);
+        return URLClassLoader.newInstance(classPathURLs,original);
+    }
+
+    private static URL toURL(Path path) {
+        try {
+            return path.toUri().toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- tester/src/test/java/com/fteotini/amf/tester/providers/JUnit5/ExecutionSummaryGenerator/TestExecutionSummaryGeneratingListenerTest.java
+++ tester/src/test/java/com/fteotini/amf/tester/providers/JUnit5/ExecutionSummaryGenerator/TestExecutionSummaryGeneratingListenerTest.java
@@ -2,11 +2,8 @@ package com.fteotini.amf.tester.providers.JUnit5.ExecutionSummaryGenerator;
 
 import com.fteotini.amf.tester.ExecutionSummary.ExecutionResult;
 import com.fteotini.amf.tester.ExecutionSummary.TestEntityType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.engine.config.JupiterConfiguration;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -16,8 +13,6 @@ import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.UniqueId;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.util.*;
 import java.util.function.Consumer;
@@ -27,7 +22,7 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.junit.platform.engine.TestDescriptor.*;
+import static org.junit.platform.engine.TestDescriptor.Type;
 import static org.mockito.Mockito.*;
 
 @SuppressWarnings("OptionalGetWithoutIsPresent")
@@ -42,8 +37,6 @@ class TestExecutionSummaryGeneratingListenerTest {
             new AbstractMap.SimpleEntry<>(TestExecutionResult.Status.SUCCESSFUL, ExecutionResult.Success),
             new AbstractMap.SimpleEntry<>(TestExecutionResult.Status.FAILED, ExecutionResult.Failure)
     );
-    @Mock
-    private JupiterConfiguration dummyConfig;
 
     private static Stream<Arguments> availableDescriptorStates() {
         return Stream.of(
@@ -58,16 +51,6 @@ class TestExecutionSummaryGeneratingListenerTest {
         return buildTestDescriptor(entityName, descriptorType, null);
     }
 
-    /*@Test
-    void Given_a_finished_execution_then_it_should_return_the_builder_result() {
-        var expected = mock(TestSuiteOutcome.class);
-        when(builder.build(any())).thenReturn(expected);
-
-        var result = finishedSut().generateTestSuiteOutcome();
-
-        assertThat(result).isEqualTo(expected);
-    }*/
-
     private static TestDescriptor buildTestDescriptor(String entityName, Type descriptorType, Function<TestDescriptor, Set<? extends TestDescriptor>> childrenSupplier) {
         var mock = mock(TestDescriptor.class);
         when(mock.getDisplayName()).thenReturn(entityName);
@@ -80,30 +63,10 @@ class TestExecutionSummaryGeneratingListenerTest {
 
             for (var child : children) {
                 when(child.getParent()).thenReturn(Optional.of(mock));
-                /*var childSegment = child.getUniqueId().getLastSegment();
-                when(child.getUniqueId()).thenReturn(mock.getUniqueId().append(childSegment));*/
             }
         }
 
         return mock;
-    }
-
-    /*@Test
-    void Given_a_test_identifier_without_parent_then_its_representation_should_be_appended_to_the_root() throws Exception {
-        var testIdentifier = TestIdentifier.from(dummyTestMethodDescriptor());
-        var sut = finishedSut(s -> s.executionStarted(testIdentifier));
-
-        var result = sut.generateTestSuiteOutcome();
-        assertThat(result.getRootTestContainers()).hasSize(1);
-        assertThat(result.getRootTestContainers().stream().findFirst().get())
-                .usingRecursiveComparison()
-                .isEqualTo(new TestNode(testIdentifier));
-    }*/
-
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.initMocks(this);
-        when(dummyConfig.getDefaultDisplayNameGenerator()).thenReturn(new DisplayNameGenerator.Standard());
     }
 
     @Test
