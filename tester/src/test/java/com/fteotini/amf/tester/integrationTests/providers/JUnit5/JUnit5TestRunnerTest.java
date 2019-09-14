71802afeddc7de7f145adeccdef859c6f861e660
Federico Teotini <templarblack.fed03@gmail.com>
2019-09-14 22:34:06 +0200

    Fixed mvn invoker build in JUnit5TestRunnerTest integrationtest

--- tester/src/test/java/com/fteotini/amf/tester/integrationTests/providers/JUnit5/JUnit5TestRunnerTest.java
+++ tester/src/test/java/com/fteotini/amf/tester/integrationTests/providers/JUnit5/JUnit5TestRunnerTest.java
@@ -8,11 +8,14 @@ import com.fteotini.amf.tester.providers.JUnit5.JUnit5TestRunnerFactory;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
@@ -22,11 +25,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JUnit5TestRunnerTest {
     private static final String SUREFIRE_VERSION = System.getProperty("surefire.version");
     private static final String COMPILER_VERSION = System.getProperty("compiler.version");
+
+    private static final String PROJECT_NAME = "JUnit5TestRunner_Integration_project";
+    private static final Path PROJECT_PATH = Path.of("src", "test", "resources", PROJECT_NAME);
+
     private static final JUnit5TestRunnerFactory testRunnerFactory = new JUnit5TestRunnerFactory();
 
+    @BeforeAll
+    static void init() throws MavenInvocationException {
+        BuildTestSubProject();
+    }
+
+    @AfterAll
+    static void dispose() throws IOException {
+        Files.walk(PROJECT_PATH.resolve("target"))
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+    }
+
     @Test
-    void it_can_run_a_suite() throws MavenInvocationException, URISyntaxException {
-        var sut = buildTestRunnerForSubProject("JUnit5TestRunner_Integration_project");
+    void it_can_run_a_suite() {
+        var sut = buildTestRunnerForSubProject();
         var result = sut.runEntireSuite().getTestContainers();
 
         assertThat(result).hasSize(4);
@@ -65,8 +85,8 @@ class JUnit5TestRunnerTest {
     }
 
     @Test
-    void Given_a_successful_run_then_result_for_With2SkippedAnd1SuccessfulTest_children_is_as_expected() throws MavenInvocationException, URISyntaxException {
-        var sut = buildTestRunnerForSubProject("JUnit5TestRunner_Integration_project");
+    void Given_a_successful_run_then_result_for_With2SkippedAnd1SuccessfulTest_children_is_as_expected() {
+        var sut = buildTestRunnerForSubProject();
 
         var testClass = getTestClassByEntityName(sut.runEntireSuite().getTestContainers(), "With2SkippedAnd1SuccessfulTest");
 
@@ -78,22 +98,22 @@ class JUnit5TestRunnerTest {
                     assertThat(e.getException()).isEmpty();
                 });
 
-        var firstSkippedTest = getTestMethodByEntityName(testClass, "it_runs_the_test");
+        var firstSkippedTest = getTestMethodByEntityName(testClass, "it_runs_the_test()");
         assertThat(firstSkippedTest.getResult()).isEqualTo(ExecutionResult.Skipped);
         assertThat(firstSkippedTest.getSkipReason()).contains("reason");
 
-        var secondSkippedTest = getTestMethodByEntityName(testClass, "it_runs_the_test_2");
+        var secondSkippedTest = getTestMethodByEntityName(testClass, "it_runs_the_test_2()");
         assertThat(secondSkippedTest.getResult()).isEqualTo(ExecutionResult.Skipped);
         assertThat(secondSkippedTest.getSkipReason()).isNotEmpty();
 
-        var successfulTest = getTestMethodByEntityName(testClass, "it_runs_the_test_3");
+        var successfulTest = getTestMethodByEntityName(testClass, "it_runs_the_test_3()");
         assertThat(successfulTest.getResult()).isEqualTo(ExecutionResult.Success);
         assertThat(successfulTest.getSkipReason()).isEmpty();
     }
 
     @Test
-    void Given_a_successful_run_then_result_for_With1FailingAnd1SuccessfulTest_children_is_as_expected() throws MavenInvocationException, URISyntaxException {
-        var sut = buildTestRunnerForSubProject("JUnit5TestRunner_Integration_project");
+    void Given_a_successful_run_then_result_for_With1FailingAnd1SuccessfulTest_children_is_as_expected() {
+        var sut = buildTestRunnerForSubProject();
 
         var testClass = getTestClassByEntityName(sut.runEntireSuite().getTestContainers(), "With1FailingAnd1SuccessfulTest");
 
@@ -105,11 +125,11 @@ class JUnit5TestRunnerTest {
                     assertThat(e.getSkipReason()).isEmpty();
                 });
 
-        var successTest = getTestMethodByEntityName(testClass, "it_runs_the_test");
+        var successTest = getTestMethodByEntityName(testClass, "it_runs_the_test()");
         assertThat(successTest.getResult()).isEqualTo(ExecutionResult.Success);
         assertThat(successTest.getException()).isEmpty();
 
-        var failingTest = getTestMethodByEntityName(testClass, "it_fails");
+        var failingTest = getTestMethodByEntityName(testClass, "it_fails()");
         assertThat(failingTest.getResult()).isEqualTo(ExecutionResult.Failure);
         assertThat(failingTest.getException())
                 .isNotEmpty()
@@ -118,8 +138,8 @@ class JUnit5TestRunnerTest {
     }
 
     @Test
-    void Given_a_successful_run_then_result_for_With2SuccessfulTest_children_is_as_expected() throws MavenInvocationException, URISyntaxException {
-        var sut = buildTestRunnerForSubProject("JUnit5TestRunner_Integration_project");
+    void Given_a_successful_run_then_result_for_With2SuccessfulTest_children_is_as_expected() {
+        var sut = buildTestRunnerForSubProject();
 
         var testClass = getTestClassByEntityName(sut.runEntireSuite().getTestContainers(), "With2SuccessfulTest");
 
@@ -150,25 +170,24 @@ class JUnit5TestRunnerTest {
     }
 
     private TestEntity getTestMethodByEntityName(TestEntity testClass, String testName) {
-        return testClass.getChildren().stream().filter(e -> e.getEntityName().startsWith(testName)).findFirst().get();
+        return testClass.getChildren().stream().filter(e -> e.getEntityName().equals(testName)).findFirst().get();
     }
 
-    private TestRunner buildTestRunnerForSubProject(String projectName) throws MavenInvocationException, URISyntaxException {
-        BuildTestSubProject(projectName);
-
-        var cp = getProjectClassPath(projectName);
+    private TestRunner buildTestRunnerForSubProject() {
+        var cp = getProjectClassPath();
         return testRunnerFactory.createTestRunner(Set.of(cp));
     }
 
-    private Path getProjectClassPath(String projectName) throws URISyntaxException {
-        return Path.of(getClass().getClassLoader().getResource(projectName + "/target/test-classes").toURI());
+    private Path getProjectClassPath() {
+        return PROJECT_PATH.resolve("target/test-classes");
     }
 
-    private void BuildTestSubProject(String projectName) throws MavenInvocationException {
-        var pomUrl = getClass().getClassLoader().getResource(projectName + "/pom.xml");
+    private static void BuildTestSubProject() throws MavenInvocationException {
+        var pomPath = PROJECT_PATH.resolve("pom.xml");
+
 
         var invocationRequest = new DefaultInvocationRequest();
-        invocationRequest.setPomFile(new File(pomUrl.getFile()));
+        invocationRequest.setPomFile(new File(pomPath.toUri()));
         invocationRequest.setBatchMode(true);
         invocationRequest.setGoals(BuildGoalsList("clean", "test-compile"));
 
