c6c90e050188eed76612dee2c3c95c27fb3116f9
Federico Teotini <templarblack.fed03@gmail.com>
2019-09-20 18:07:49 +0200

    ProcessInvoker tested

--- launcher/src/main/java/com/fteotini/amf/launcher/process/ProcessInvoker.java
+++ launcher/src/main/java/com/fteotini/amf/launcher/process/ProcessInvoker.java
@@ -5,6 +5,7 @@ import org.zeroturnaround.exec.ProcessExecutor;
 import org.zeroturnaround.exec.ProcessResult;
 import org.zeroturnaround.exec.stream.ExecuteStreamHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +18,7 @@ public class ProcessInvoker {
     private final Supplier<ProcessExecutor> processExecutorSupplier;
 
     public ProcessInvoker(ProcessArgs processArgs, ExecuteStreamHandler processCommunicationHandler) {
-        this(processArgs,processCommunicationHandler, ProcessExecutor::new);
+        this(processArgs, processCommunicationHandler, ProcessExecutor::new);
     }
 
     ProcessInvoker(ProcessArgs processArgs, ExecuteStreamHandler processCommunicationHandler, Supplier<ProcessExecutor> processExecutorSupplier) {
@@ -26,10 +27,13 @@ public class ProcessInvoker {
         this.processExecutorSupplier = processExecutorSupplier;
     }
 
-    public Future<ProcessResult> startMinionProcess(){
+    public Future<ProcessResult> startMinionProcess() throws IOException {
         var command = getArgsList();
-        processExecutorSupplier.get().command(command);
-        return null;
+        return processExecutorSupplier.get()
+                .command(command)
+                .streams(processCommunicationHandler)
+                .start()
+                .getFuture();
     }
 
     private List<String> getArgsList() {
@@ -37,33 +41,5 @@ public class ProcessInvoker {
         args.add(MinionEntryPoint.class.getCanonicalName());
         return args;
     }
-
-    /*public static void main(String[] args) throws InterruptedException, TimeoutException, IOException, ClassNotFoundException {
-        var inputMsg = new DTO(MinionEntry.class);
-        var input = new InputStreamWriter()
-                .writeObject(inputMsg)
-                .create();
-
-        var cl = System.getProperty("java.class.path") + ";D:/Code/SWAM/amf/launcher/target/test-classes";
-
-
-        var d2 = "-agentlib:jdwp=transport=dt_socket,server=n,address=DESKTOP-TR4EENN:50005,suspend=y";
-
-        var output = new OutputStreamReader();
-
-        var processExecutor = new ProcessExecutor("java", d2,"-cp", cl, "DummyMain");
-        var r = processExecutor
-                .redirectInput(input)
-                .redirectOutput(output.getOutputStream())
-                .redirectError(System.err)
-//                .readOutput(true)
-//                .destroyOnExit()
-                .execute();
-
-        var summ = output.readObject(TestExecutionSummary.class);
-        System.out.println("output: "+summ.getTestContainers().size());
-        System.out.println(r.getExitValue());
-        //System.out.println(r.outputString());
-    }*/
 }
 
--- launcher/src/test/java/com/fteotini/amf/launcher/process/ProcessInvokerTest.java
+++ launcher/src/test/java/com/fteotini/amf/launcher/process/ProcessInvokerTest.java
@@ -1,6 +1,8 @@
 package com.fteotini.amf.launcher.process;
 
 import com.fteotini.amf.launcher.minion.MinionEntryPoint;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -12,13 +14,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
+import org.zeroturnaround.exec.StartedProcess;
 import org.zeroturnaround.exec.stream.ExecuteStreamHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @Tag("UnitTest")
 @ExtendWith(MockitoExtension.class)
@@ -30,15 +35,18 @@ class ProcessInvokerTest {
     private ExecuteStreamHandler commsHandler;
     @Mock
     private ProcessArgs processArgs;
+    @Mock
+    private StartedProcess startedProcess;
     private ProcessInvoker sut;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        when(processExecutor.start()).thenReturn(startedProcess);
         sut = new ProcessInvoker(processArgs,commsHandler,() ->processExecutor);
     }
 
     @Test
-    void Given_a_processArgs_obj_then_it_should_build_the_command_correctly() {
+    void Given_a_processArgs_obj_then_it_should_build_the_command_correctly() throws IOException {
         var command = List.of("foo","bar");
         when(processArgs.buildArgsList()).thenReturn(command);
 
@@ -47,5 +55,21 @@ class ProcessInvokerTest {
         var expectedCommand = new ArrayList<>(command);
         expectedCommand.add(MinionEntryPoint.class.getCanonicalName());
         verify(processExecutor).command(expectedCommand);
+    }
+
+    @Test
+    void When_startMinionProcess_then_it_should_pass_the_comms_handler_to_the_process_builder() throws IOException {
+        sut.startMinionProcess();
+        verify(processExecutor).streams(commsHandler);
+    }
+
+    @Test
+    void When_startMinionProcess_then_its_output_should_be_the_same_as_the_process() throws IOException {
+        var expected = Futures.immediateFuture(new ProcessResult(2, null));
+        when(startedProcess.getFuture()).thenReturn(expected);
+
+        var result = sut.startMinionProcess();
+
+        assertThat(result).isEqualTo(expected);
     }
 }
