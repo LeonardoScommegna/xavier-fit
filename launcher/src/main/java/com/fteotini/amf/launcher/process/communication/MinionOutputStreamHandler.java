988775b3a6a4deabb57b73142867fe9c15e29ffc
Federico Teotini <templarblack.fed03@gmail.com>
2019-09-22 21:52:42 +0200

    SendInitial data class

--- launcher/src/main/java/com/fteotini/amf/launcher/process/communication/MinionOutputStreamHandler.java
+++ launcher/src/main/java/com/fteotini/amf/launcher/process/communication/MinionOutputStreamHandler.java
@@ -5,10 +5,10 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 
-public class MinionOutputStreamHandler {
+class MinionOutputStreamHandler {
     private final OutputStream os;
 
-    public MinionOutputStreamHandler(final OutputStream os) {
+    MinionOutputStreamHandler(final OutputStream os) {
         this.os = os;
     }
 
--- launcher/src/main/java/com/fteotini/amf/launcher/process/communication/SendInitialData.java
+++ launcher/src/main/java/com/fteotini/amf/launcher/process/communication/SendInitialData.java
@@ -1,10 +1,25 @@
 package com.fteotini.amf.launcher.process.communication;
 
+import com.fteotini.amf.launcher.minion.MinionArgs;
+import com.google.common.base.Throwables;
+
+import java.io.IOException;
 import java.util.function.Consumer;
 
 public class SendInitialData implements Consumer<MinionOutputStreamHandler> {
+
+    private final MinionArgs initialData;
+
+    public SendInitialData(final MinionArgs initialData) {
+        this.initialData = initialData;
+    }
+
     @Override
     public void accept(MinionOutputStreamHandler minionOutputStreamHandler) {
-        throw new RuntimeException("Not Implemented");
+        try {
+            minionOutputStreamHandler.writeObject(initialData);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
