e7b3a05f98e0dfdc29e26a5a24671b22477ebd74
Federico Teotini <templarblack.fed03@gmail.com>
2019-10-04 12:54:04 +0200

    ReceiveData handles result reading

--- launcher/src/main/java/com/fteotini/amf/launcher/process/communication/ReceiveData.java
+++ launcher/src/main/java/com/fteotini/amf/launcher/process/communication/ReceiveData.java
@@ -1,12 +1,24 @@
 package com.fteotini.amf.launcher.process.communication;
 
+import com.fteotini.amf.commons.tester.ExecutionSummary.TestExecutionSummary;
 import com.fteotini.amf.launcher.MinionInputStreamHandler;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 public class ReceiveData implements Consumer<MinionInputStreamHandler> {
+    private TestExecutionSummary result;
+
     @Override
     public void accept(MinionInputStreamHandler inputStreamHandler) {
-        throw new RuntimeException("Not Implemented");
+        try {
+            result = inputStreamHandler.readObject(TestExecutionSummary.class);
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public TestExecutionSummary receive() {
+        return result;
     }
 }
