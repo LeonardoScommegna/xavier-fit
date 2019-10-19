cff5dccba28a31e27629b4d0c1ffce9810003481
Federico Teotini <templarblack.fed03@gmail.com>
2019-10-19 19:07:48 +0200

    Beginning of provided mutator implementations

---
+++ provided-mutators/src/main/java/com/fteotini/amf/providedMutators/DefaultModule.java
@@ -0,0 +1,13 @@
+package com.fteotini.amf.providedMutators;
+
+import com.fteotini.amf.mutator.Container.MutatorModule;
+import com.fteotini.amf.mutator.MutatorsBuilder;
+
+import java.util.List;
+
+public class DefaultModule implements MutatorModule {
+    @Override
+    public List<MutatorsBuilder> registerAdditionalMutators() {
+        return null;
+    }
+}
