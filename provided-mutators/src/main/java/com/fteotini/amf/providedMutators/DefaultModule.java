4a4e37387632f757498c058b541df85c50af53f4
Federico Teotini <templarblack.fed03@gmail.com>
2019-10-20 18:18:02 +0200

    Registered provided mutators into default module

--- provided-mutators/src/main/java/com/fteotini/amf/providedMutators/DefaultModule.java
+++ provided-mutators/src/main/java/com/fteotini/amf/providedMutators/DefaultModule.java
@@ -3,6 +3,8 @@ package com.fteotini.amf.providedMutators;
 import com.fteotini.amf.mutator.Container.MutatorModule;
 import com.fteotini.amf.mutator.MutatorsBuilder;
 import com.fteotini.amf.providedMutators.CDI.SwitchSessionToRequestScopedOnType;
+import com.fteotini.amf.providedMutators.JPA.RemoveGeneratedValueOnField;
+import com.fteotini.amf.providedMutators.JaxRS.ReplaceProducesValueOnMethod;
 
 import java.util.List;
 
@@ -10,7 +12,9 @@ public class DefaultModule implements MutatorModule {
     @Override
     public List<MutatorsBuilder> registerAdditionalMutators() {
         return List.of(
-                new SwitchSessionToRequestScopedOnType()
+                new SwitchSessionToRequestScopedOnType(),
+                new ReplaceProducesValueOnMethod(),
+                new RemoveGeneratedValueOnField()
         );
     }
 }
