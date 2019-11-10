8a050319f00226e12233eabc6ca651a94207043e
Federico Teotini <templarblack.fed03@gmail.com>
2019-11-10 17:02:01 +0100

    Moved integration module to its branch

--- maven-plugin-integration/src/test/resources/javaee-app/src/main/java/it/fed03/config/RestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package it.fed03.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestConfiguration extends Application {
-}
